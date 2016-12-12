7486049e80df7bb2f2ca9e7f7444dad99ce1cf2e
Joshua Fong <jwfong89@gmail.com>
2016-12-11 17:55:17 -0800

    added 2.2, 2.3, 2.4; return kth, deleteMiddleNode, and partition

--- Chapter2-Linked-Lists.js
+++ Chapter2-Linked-Lists.js
@@ -1,10 +1,14 @@
 let LinkedList = function() {
   this.head = null;
   this.tail = null;
+  //added before 2.3
+  this.size = 0;
 };
 
 LinkedList.prototype.addToTail = function(value) {
   let newTail = Node(value);
+  // increase size everytime a node is added
+  this.size++;
 
   if (!this.head) {
     this.head = newTail;
@@ -24,7 +28,10 @@ LinkedList.prototype.removeHead = function() {
   
   let currentHead = this.head;
   this.head = this.head.next;
+  //decrease size whenever head is removed
+  this.size--;
 
+  
   return currentHead.value;
 };
 
@@ -62,6 +69,92 @@ LinkedList.prototype.removeDuplicates = function() {
 }
 
 
+//2.1 Return Kth to Last O(n) time O(n) space
+LinkedList.prototype.kthToLast = function(k) {
+  let node = this.head;
+  //find size
+  let size = 0;
+  let hash = [];
+  let nodePosition = size - k;
+  
+  while (node !== null) {
+    size++;
+    hash.push(node.value);
+    node = node.next;
+  }
+  return hash[nodePosition];
+  
+  ////////// code below could be used to achieve O(1) space //////////
+
+  // let traverseCount = 0;
+  // node = this.head;
+  // while (traverseCount !== nodePosition) {
+  //   node = node.next;
+  //   traverseCount++;
+  // }
+
+  // return node.value;
+}
+
+// 2.3 Delete Middle Node
+// added size property to linked list
+// seemed to be easier than finding size every time
+LinkedList.prototype.deleteMiddleNode = function() {
+  let node = this.head;
+  let middle = Math.ceil(this.size/2);
+  let count = 1;
+  while (count !== middle - 1) {
+    count++;
+    node = node.next
+  }
+  let temp = node.next;
+  node.next = node.next.next;
+  this.size--;
+
+  // I chose to return the value being remove 
+  return temp;
+}
+
+
+// LinkedList.prototype.delete = function(val) {
+//   let node = this.head;
+
+//   while (node !== null) {
+//     if (node.next.value === val) {
+//       node.next = node.next.next;
+//       this.size--;
+//       return;
+//     }
+//     node = node.next;
+//   }
+// }
+
+// 2.4 Partition 
+
+LinkedList.prototype.partition = function(val) {
+  let node = this.head;
+
+  let lowerHalf = new LinkedList;
+  let higherHalf = new LinkedList;
+
+  while (node !== null) {
+    if (node.value < val) {
+      lowerHalf.addToTail(node.value);
+    }
+    if (node.value >= val) {
+      higherHalf.addToTail(node.value);
+    }
+    node = node.next;
+  }
+  
+  lowerHalf.tail.next = higherHalf.head;
+  lowerHalf.size = this.size;
+  lowerHalf.tail = higherHalf.tail;
+  
+  return lowerHalf;
+
+}
+
 let Node = function(value) {
   let node = {};
   node.value = value;
@@ -78,8 +171,9 @@ newLL.addToTail(10);
 newLL.addToTail(3);
 newLL.addToTail(4);
 newLL.addToTail(1);
-newLL.addToTail(1);
-newLL.addToTail(1);
+newLL.addToTail(11);
+newLL.addToTail(100);
+newLL.addToTail(12);
 newLL.addToTail(5);
 newLL.addToTail(6);
 newLL.addToTail(7);
